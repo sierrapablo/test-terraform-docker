430832ee3fa0d8bfc7e9884ec18f9f4d4b08ecb4
sierrapablo <proyectos@sierrapablo.dev>
2025-11-13 21:21:20 +0100

    ✨ add feature : add outputs to pipeline

---
+++ terraform/outputs.tf
@@ -0,0 +1,15 @@
+output "container_name" {
+  description = "The name of the Ubuntu container"
+  value       = docker_container.ubuntu.name
+}
+
+output "container_id" {
+  description = "The Docker container ID"
+  value       = docker_container.ubuntu.id
+}
+
+output "container_ip" {
+  description = "The IP address of the container (if needed)"
+  value       = docker_container.ubuntu.network_data[0].ip_address
+  // Esto solo funciona si usas el network "bridge" y el contenedor ya tiene IP asignada
+}
