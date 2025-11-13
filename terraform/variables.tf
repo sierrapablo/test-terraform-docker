da3f6dfc1fc8930ca90a0506471125d3f2ee0d82
sierrapablo <proyectos@sierrapablo.dev>
2025-11-13 20:53:56 +0100

    🔨 fix bugs : remove unused variables due to locals

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,20 +2,3 @@ variable "machine_size" {
   description = "Machine size label (s, m, l, etc.)"
   type        = string
 }
-
-variable "cpu" {
-  description = "CPU shares"
-  type        = number
-  default     = 1024
-}
-
-variable "memory" {
-  description = "Memory limit in bytes"
-  type        = number
-  default     = 512000000
-}
-
-variable "container_suffix" {
-  description = "Unique suffix for container name"
-  type        = string
-}
